be793611edb4e831d4b2eba3c027aed4e781d4b7
snntaylan <sinan.taylan@outlook.com>
2019-04-08 08:02:47 +0300

    second commit

--- vue_api/src/main.js
+++ vue_api/src/main.js
@@ -1,8 +1,15 @@
-import Vue from 'vue'
-import App from './App.vue'
-
-Vue.config.productionTip = false
-
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  el: "#vue-app",
+  data: {
+    name: "",
+    age: ""
+  },
+  methods: {
+    logName: function() {
+      console.log("write your name");
+    },
+    logAge: function() {
+      console.log("write your age");
+    }
+  }
+});
